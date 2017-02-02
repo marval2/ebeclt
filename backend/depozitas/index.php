12fb75b0dbcfd15662b67a04c5e1d013260a9b79
Slivinskas <slivinskasvytenis@gmail.com>
2017-02-02 23:59:54 +0200

    Stilistiniai pataisymai Atsijungimas

--- backend/depozitas/index.php
+++ backend/depozitas/index.php
@@ -15,41 +15,42 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
+$content = "";
 
 if(isset($_POST["login"])){
     if(!empty($_POST["user"]) && ! empty($_POST["pass"])){
         if($_POST["user"]== "kiekviena" && $_POST["pass"]== "mumSekasi84"){
             $_SESSION["prisijunges"] = "prisijungta";
-            echo "Prisijungta";
         }else{
             echo "Blogas prisijungimo vardas arba slaptažodis";
         }
     }
+}elseif(isset($_POST["logout"])){
+    unset($_SESSION["prisijunges"]);
 }
 
 if(empty($_SESSION["prisijunges"])){
     $content =  "
-        <form method='post'>
+<form method='post' id='loginForm'>
+    <h1>Prisijungti</h1>
     <input type='text' name='user' placeholder='Prisijungimo vardas'>
     <input type='password' name='pass' placeholder='Slaptažodis'>
     <input type='submit' name='login' value='Prisijungti'>
 </form>
     ";
 }else{
-    $content = "";
+
     $config = include_once ("../_config.php");
     $db = new DeposidDB($config->username,$config->pass,$config->database);
     if(isset($_POST["id"]) && $_POST["id"] > 0){
         $checked = $_POST["approved"] == "true" ? 5 : 0;
         $rez = $db->updateAplicant($checked,$_POST["id"]);
-        $content = $rez === true ? "Statusas pakeistas" : "Klaida:" . $rez;
+        echo $rez === true ? "Statusas pakeistas" : "Klaida:" . $rez;
         exit();
     }else {
-        $content = renderApplicantList($db->applicantsList());
+        $content .= "<form method='post'><input id='logout' type='submit' name='logout' value='Atsijungti'></form>".renderApplicantList($db->applicantsList());
     }
 }
-
-
 ?>
 
 <html>
