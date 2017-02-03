ab958f8d543f1f86d562ae4a471a154f6bd94195
Marius <marius.krajauskas@gmail.com>
2017-02-03 10:19:35 +0200

    Timeris pakeistas i 24 dienos 8h ryto

--- js/coundown-timer.js
+++ js/coundown-timer.js
@@ -5,7 +5,7 @@
 		thisEl = $(this);
 
 		//array of custom settings
-		var settings = { 
+		var settings = {
 			'date': "2017-02-24",
 			'format': null
 		};
@@ -14,13 +14,13 @@
 		if(options) {
 			$.extend(settings, options);
 		}
-		
+
 		//main countdown function
 		function countdown_proc() {
-			
-			eventDate = new Date('2017-02-24').getTime() / 1000;
+
+			eventDate = new Date('2017-02-24 08:00:00.000').getTime() / 1000;
 			currentDate = Math.floor(new Date().getTime() / 1000);
-			
+
 			if(eventDate <= currentDate) {
 				callback.call(this);
 				clearInterval(interval);
@@ -29,19 +29,19 @@
 			seconds = eventDate - currentDate;
 			days = Math.floor(seconds / (60 * 60 * 24)); //calculate the number of days
 			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of days removed
-			
+
 			hours = Math.floor(seconds / (60 * 60));
 			seconds -= hours * 60 * 60; //update the seconds variable with no. of hours removed
-			
+
 			minutes = Math.floor(seconds / 60);
 			seconds -= minutes * 60; //update the seconds variable with no. of minutes removed
-			
+
 			//conditional Ss
 			if (days == 1) { thisEl.find(".timeRefDays").text("day"); } else { thisEl.find(".timeRefDays").text("days"); }
 			if (hours == 1) { thisEl.find(".timeRefHours").text("hour"); } else { thisEl.find(".timeRefHours").text("hours"); }
 			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
 			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
-			
+
 			//logic for the two_digits ON setting
 			if(settings['format'] == "on") {
 				days = (String(days).length >= 2) ? days : "0" + days;
@@ -49,24 +49,24 @@
 				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
 				seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
 			}
-			
+
 			//update the countdown's html values.
 			if(!isNaN(eventDate)) {
 				thisEl.find(".days").text(days);
 				thisEl.find(".hours").text(hours);
 				thisEl.find(".minutes").text(minutes);
 				thisEl.find(".seconds").text(seconds);
-			} else { 
+			} else {
 				alert("Invalid date. Here's an example: 12 Tuesday 2012 17:30:00");
-				clearInterval(interval); 
+				clearInterval(interval);
 			}
 		}
-		
+
 		//run the function
 		countdown_proc();
-		
+
 		//loop the function
 		interval = setInterval(countdown_proc, 1000);
-		
+
 	}
 }) (jQuery);
